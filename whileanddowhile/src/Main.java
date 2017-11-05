a9a7610c8d76ae991e3616b1cb57a9248bd729a1
najeex <najeebagra@gmail.com>
2017-11-05 17:29:41 +0530

    add just new java program

--- whileanddowhile/src/Main.java
+++ whileanddowhile/src/Main.java
@@ -1,19 +1,41 @@
 public class Main {
 
-    public static void main(String[] args){
-        int count =0;
-        while(count !=5){
-            System.out.println("count value is "+ count);
-            count++;
-        }
+    public static void main(String[] args) {
+//        int count =0;
+//        while(count !=5){
+//            System.out.println("count value is "+ count);
+//            count++;
+//        }
+//
+//        count =1;
+//        while(true){
+//            if(count ==6){
+//                break;
+//            }
+//            System.out.println("count value is " + count);
+//            count++;
 
-        count =1;
-        while(true){
-            if(count ==6){
-                break;
-            }
-            System.out.println("count value is " + count);
-            count++;
+    int number =5;
+    int finisnumber = 20;
+    while(number <= finisnumber){
+        if(!isevennumber(number)){
+            number++;
+            continue;
+        }
+        System.out.println("even number "+ number);
+        number++;
+    }
+
+
+    }
+
+    public static boolean isevennumber(int number){
+        if((number % 2) == 0){
+            return true;
+
+        }else{
+            return false;
         }
     }
 }
+
